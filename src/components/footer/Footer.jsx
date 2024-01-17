06d0137d6ecdd9380b68377163a4a31df6d6eb7d
Abdul Mohiz <fatizforever@gmail.com>
2024-01-17 22:05:16 +0500

    Home page And Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,15 @@
+import styles from "./footer.module.css"
 const Footer = () => {
-    return (
-    <div>
-      Footer
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        Almohiz
+      </div>
+      <div className={styles.text}>
+        Almohiz creative thoughts
+      </div>
     </div>
-    )
-  };
-  
-  export default Footer;
+  )
+};
+
+export default Footer;
