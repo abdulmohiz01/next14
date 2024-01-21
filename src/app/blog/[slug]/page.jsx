162637dd1a0064073a25fc9f7ac50b3420e1f9e7
Abdul Mohiz <fatizforever@gmail.com>
2024-01-21 17:34:03 +0500

    Blog page update

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-
+import styles from "./singlepost.module.css"
+import Image from "next/image"
 const SinglePostPage = () => {
   return (
-    <div>SinglePostPage</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="https://images.pexels.com/photos/9162803/pexels-photo-9162803.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image src="https://images.pexels.com/photos/9162803/pexels-photo-9162803.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width={50} height={50} className={styles.avatar} />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Abdul Mohiz</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>21/01/2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. A itaque distinctio delectus ut nobis ea quia quibu
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -6,14 +6,14 @@ const PostCard = () => {
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.imgContainer}>
-          <Image src="/post.png" fill alt="" className={styles.img} />
+          <Image src="https://images.pexels.com/photos/9162803/pexels-photo-9162803.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" fill alt="" className={styles.img} />
         </div>
         <span className={styles.date}>01.01.2024</span>
       </div>
       <div className={styles.bottom}>
         <h1 className={styles.title}>Title</h1>
-        <p className={styles.desc}>description</p>
-        <Link href="/blog/post">Read</Link>
+        <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt impedit hic, error blanditiis ipsum cupiditate fugit quas fuga</p>
+        <Link  className={styles.link} href="/blog/post">Read More</Link>
       </div>
     </div>
   )
